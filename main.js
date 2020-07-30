27feb6f608c57ebf6c9a79828fdd22713f4c68b8
naomi <iam.naomi92@gmail.com>
2020-07-30 22:40:34 +0900

    challenge7

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /*   マイルストーン１   */
 // alertで'You got 5 guesses. Guess 1 to 5'と言うメッセージを表示させ、１から５までの数値を当てるよう指示する。
-alert('You got 5 guesses. Guess 1 to 5');
+ alert('You got 5 guesses. Guess 1 to 5');
 
 // 答えの数値を2で用意
 let answer = 2;
@@ -11,9 +11,12 @@ prompt('What do you guess?');
 // ヒント１：if文とも組み合わせて、推測が当たった場合はalertで'Good job! See what happens next :)'を表示。当たったらゲーム終了にする。
 // ヒント２：外れたらpromptで'Ops! Maybe, another try?'のメッセージとともに記入欄を表示。
 for (let i = 1; i<= 5; i++) {
-    if (i === 5){
-
-    }
+     if (i === 2){
+        alert('Good job! See what happens next :)');
+        break;
+     } else {
+        prompt('Ops! Maybe, another try?');
+     }
 }
 
 
