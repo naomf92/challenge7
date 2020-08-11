3b2807eb850b55dac3daa1a556aba0eb34ba949e
naomi <iam.naomi92@gmail.com>
2020-08-11 21:09:45 +0900

    challenge7

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-const img = document.getElementById('img');
-// img.innerHTML = 
-console.log(img);
-
 /*   マイルストーン１   */
 // alertで'You got 5 guesses. Guess 1 to 5'と言うメッセージを表示させ、１から５までの数値を当てるよう指示する。
  alert('You got 5 guesses. Guess 1 to 5');
@@ -17,40 +13,51 @@ let message = prompt('What do you guess?');
 const answer = [2];
 let array = [1,3,4,5];
 
-for (let i = 0; i<=4; i++) {
-    //else ifの箇所は、実行されずに素通りしている可能性があるので、ここでarray[i]の値を検証します
-    // 正解以外、どの数値をブラウザで入れても1, 3, 4, 5が出てきます
-    // 配列arrayの値の中身が順番につずつ出てきていることがわかります
-    // 1つ数字をブラウザで入力するたびに、配列arrayの先頭の値1つずつしか対象にしてくれないようです。
-    // array[i]とすると、思うように動いてくれないみたいなので、array[指定インデックス番号]で書くと良さそうです
-    // console.log(array[i]);
-    if (message == answer){
-       alert('Good job! See what happens next :)');
-       break;
-    } else if (
-       message == array[0] ||
-       message == array[1] ||
-       message == array[2] ||
-       message == array[3] ||
-    ) {
-       message = prompt('Ops! Maybe, another try?');
+// for (let i = 0; i<=4; i++) {
+//     //else ifの箇所は、実行されずに素通りしている可能性があるので、ここでarray[i]の値を検証します
+//     // 正解以外、どの数値をブラウザで入れても1, 3, 4, 5が出てきます
+//     // 配列arrayの値の中身が順番につずつ出てきていることがわかります
+//     // 1つ数字をブラウザで入力するたびに、配列arrayの先頭の値1つずつしか対象にしてくれないようです。
+//     // array[i]とすると、思うように動いてくれないみたいなので、array[指定インデックス番号]で書くと良さそうです
+//     // console.log(array[i]);
+//     if (message == answer){
+//       alert('Good job! See what happens next :)');
+//       const img = document.getElementById('img');
+//       img.innerHTML = `<h1>You made it! 🎮</h1><iframe src="https://giphy.com/embed/11sBLVxNs7v6WA" width="480" height="217" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/cheer-cheering-11sBLVxNs7v6WA"></a></p> `
+//       break;
+//     } else if (
+//        message == array[0] ||
+//        message == array[1] ||
+//        message == array[2] ||
+//        message == array[3] 
+//     ) {
+//        message = prompt('Ops! Maybe, another try?');
 
-    } else {
-       message = prompt('Pick a number from 1 to 5');
-    }
-}
+//     } else {
+//        message = prompt('Pick a number from 1 to 5');
+//     }
+// }
 
 /*   マイルストーン2   */
 // for文で完成したら、for文箇所をコメントアウトし、while文で書き換えてみましょう。
 
-// let i = 0;
-// while (i<=4) {
-//    i++;
-//    if (message == 2){
-//       alert('Good job! See what happens next :)');
-//       break;
-//    } else {
-//       message = prompt('Ops! Maybe, another try?');
-//    }
-//    console.log(message);
-// }
+let i = 0;
+while (i<=4) {
+   i++;
+   if (message == answer){
+      alert('Good job! See what happens next :)');
+      const img = document.getElementById('img');
+      img.innerHTML = `<h1>You made it! 🎮</h1><iframe src="https://giphy.com/embed/11sBLVxNs7v6WA" width="480" height="217" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/cheer-cheering-11sBLVxNs7v6WA"></a></p> `
+      break;
+   } else if (
+      message == array[0] ||
+      message == array[1] ||
+      message == array[2] ||
+      message == array[3]
+   ) {
+      message = prompt('Ops! Maybe, another try?');
+   } else  {
+      message = prompt('Pick a number from 1 to 5');
+   }
+}
+
